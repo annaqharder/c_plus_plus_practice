b6f545d33d19c056bd4a364b0bbbd881f15d61fe
Anna Harder <annaqharder@gmail.com>
2023-07-16 17:54:14 -0600

    practiced variables

---
+++ basics_level_1/hello.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    // 1) declaring a variable
+    int a; 
+    // 2) initializing the variable
+    a = 2;
+
+    // declare & initialize variable at the same time
+    int b = a + 3; 
+
+     std::cout << b << std::endl;
+
+ 
+    return 0;
+}
