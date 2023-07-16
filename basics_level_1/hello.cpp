195e10ad6e2b7aff27192fc54dcaed0612f0b665
Anna Harder <annaqharder@gmail.com>
2023-07-16 17:59:11 -0600

    data types

--- basics_level_1/hello.cpp
+++ basics_level_1/hello.cpp
@@ -1,17 +1,41 @@
 #include <iostream>
 
+// int main()
+// {
+//     // 1) declaring a variable
+//     int a;
+//     int temperature;
+//     int user_age;
+//     // 2) initializing the variable
+//     a = 2;
+
+//     // declare & initialize variable at the same time
+//     int b = a + 3; 
+
+//     std::cout << b << std::endl;
+
+//     return 0;
+// }
+
 int main()
 {
-    // 1) declaring a variable
-    int a; 
-    // 2) initializing the variable
-    a = 2;
+    // integer
+    int user_age = 34;
 
-    // declare & initialize variable at the same time
-    int b = a + 3; 
+    // float 
+    double temperature = 20.6;
 
-     std::cout << b << std::endl;
+    // boolean; false = 0; true = 1
+    bool is_alive = false;
 
- 
+    // string
+    std::string user_name = "Bob";
+
+
+    std::cout << user_age << std::endl;
+    std::cout << temperature << std::endl;
+    std::cout << is_alive << std::endl;
+    std::cout << user_name << std::endl;
+
     return 0;
 }
